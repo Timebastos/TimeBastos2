19a527b5ce495e84b93bc201aea80cbe50328d6a
Timebastos <timebastos@gmail.com>
2025-04-24 09:07:56 -0300

    Add files via upload

---
+++ treinador_bastos_app_v3.py
@@ -0,0 +1,5 @@
+
+# Código atualizado do app com prescrição quinzenal
+# Este espaço será preenchido com o conteúdo correto do canvas
+
+# Instrução: copie o conteúdo diretamente do canvas ou acione nova leitura se necessário
