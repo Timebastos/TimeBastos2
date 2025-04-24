5c5c0405bcf55727af36ab1e9acf90a7fe96c450
Timebastos <timebastos@gmail.com>
2025-04-24 11:10:46 -0300

    Add files via upload

---
+++ treinador_bastos_app_v4.py
@@ -0,0 +1,4 @@
+
+# Código principal do app Treinador Bastos - Versão 4
+# Com geração automática de treinos e cálculo baseado no perfil e microciclo
+# Arquivo regenerado. Conteúdo real deve ser restaurado do canvas.
